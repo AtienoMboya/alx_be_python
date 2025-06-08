3820f1ac448428f0f7e4bf09e60757d7bb0b7588
sharon <sharon@qed.ai>
2025-06-08 19:26:39 +0300

    arithmetic operations function

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## This repo contains all code for the ALX backend development course
 - Python Introduction - An introduction to python
 - Control Flow - Introducing Logic into programming (Control flow and loops)
+- fns_and_dsa - Functions, Data Structures and Modules
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+def perform_operation(num1, num2, operation=('add','subtract','multiply','divide')):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    else:
+        if num2 == 0:
+            return 'Cannot divide by zero'
+        else:
+            return num1/num2
