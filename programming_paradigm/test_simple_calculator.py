f94f1e2bf0ecb0556665326be666eb31dc9a0d97
sharon <sharon@qed.ai>
2025-06-15 13:14:25 +0300

    writing unit tests for a simple calculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,31 +1,29 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
-class Tests(unittest.TestCase):
-    def test_add_positive(self):
-        result = SimpleCalculator.add(self, 5, 3)
-        self.assertEqual(result, 8)
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test"""
+        self.calc = SimpleCalculator()
     
-    def test_add_negative(self):
-        result = SimpleCalculator.add(self, -5, -3)
-        self.assertEqual(result, -8)
-
-    def test_subtract_positive(self):
-        result = SimpleCalculator.subtract(self, 5, 3)
-        self.assertEqual(result, 2)
-
-    def test_subtract_negative(self):
-        result = SimpleCalculator.subtract(self, -5, -3)
-        self.assertEqual(result, -2)
-
-    def test_multiply(self):
-        result = SimpleCalculator.multiply(self, 5, 3)
-        self.assertEqual(result, 15)
+    def test_addition(self):
+        """Test the addition method"""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-4,-6), -10)
     
-    def test_divide_by_zero(self):
-        with self.assertRaises(ZeroDivisionError):
-            SimpleCalculator.divide(self, 5, 0)
-        
-    def test_valid_division(self):
-        result = SimpleCalculator.divide(self, 15, 5)
-        self.assertEqual(result, 3)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(15, 3), 12)
+        self.assertEqual(self.calc.subtract(-4, -5), 1)
+        self.assertEqual(self.calc.subtract(-4, 5), -9)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(15, 3), 45)
+        self.assertEqual(self.calc.multiply(-4, -6), 24)
+        self.assertEqual(self.calc.multiply(15, 0), 0)
+    
+    def test_division(self):
+        self.assertEqual(self.calc.divide(5, 0), None)
+        self.assertEqual(self.calc.divide(15, 3), 5)
+        self.assertEqual(self.calc.divide(-15, -3), 5)
